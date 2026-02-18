0ab027acc7a11810845b7f72e3d2dd9dd106dfae
Marcy Student <Marcy_Student@MARCY-MBAir40.local>
2026-02-18 16:00:09 -0500

    completed fetching for my single art

--- mod-4-project/src/fetch-helpers.js
+++ mod-4-project/src/fetch-helpers.js
@@ -14,3 +14,21 @@ export const getCollection = async() => {
         return { data: null, error: error };
     }
 }
+
+
+
+export const getSingleArt = async (id) =>{
+    try{
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`)
+        
+        if(!response.ok){
+            throw new Error("Failed to fetch art!")
+        }
+        const result = await response.json()
+        return { data: result.data, error: null}
+    }
+    catch(error){
+        console.warn(error.message)
+        return { data: null, error };
+    }
+};
