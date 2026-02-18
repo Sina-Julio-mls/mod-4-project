8b5d3a1811007d9507f6f643e77a77d163fd93d9
Julioj1 <dbjulioj@gmail.com>
2026-02-18 15:15:44 -0500

    Still working on the renderCollection function

---
+++ mod-4-project/mod-4-project/src/dom-helpers.js
@@ -0,0 +1,10 @@
+export const renderCollection = (collection) => {
+    const collectionList = document.querySelector('#collection-list');
+
+    collection.forEach((painting) => {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const title = document.createElement('h3');
+        const 
+    })
+}
